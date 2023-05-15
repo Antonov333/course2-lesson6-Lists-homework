96f64a0c708f7437a019eec42d0b0f61dfa77958
Antonov333 <antonovsergei333@yahoo.com>
2023-05-15 23:30:15 +0300

    ListsController and ListsService classes created

--- src/main/java/pro/sky/course2lesson6listshomework/ListsController.java
+++ src/main/java/pro/sky/course2lesson6listshomework/ListsController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ListsController {
 
+    ListsService listsService = new ListsService();
+
     @GetMapping()
     public String welcome() {
-        return "Hi!";
+        return listsService.welcome();
     }
 
 }
